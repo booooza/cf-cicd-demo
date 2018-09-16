faf4bddad63168aeb8412cff8d711c0be5b2d6a0
Marc Bischof <marc.bischof@swisscom.com>
2018-09-16 23:14:28 +0200

    Update IBM Bluemix cf commands

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Download IBM Bluemix cli and login
 curl -fsSL https://clis.ng.bluemix.net/install/linux | sh
 bluemix api https://api.ng.bluemix.net
 bluemix login -u $BLUEMIX_USER -p $BLUEMIX_PASS -o $BLUEMIX_ORGANIZATION -s $BLUEMIX_SPACE
@@ -8,15 +9,15 @@ bluemix login -u $BLUEMIX_USER -p $BLUEMIX_PASS -o $BLUEMIX_ORGANIZATION -s $BLU
 bluemix cf push Green -n cf-react-cicd-demo-new --docker-image booooza/cf-react-cicd-demo:latest
 
 # Map original route to new app
-bluemix cf map-route Green cf-react-cicd-demo.mybluemix.net -n cf-react-cicd-demo
+bluemix cf map-route Green mybluemix.net -n cf-react-cicd-demo
 
 # TODO: Verify new app is running as expected
 # Unmap route to old app
-bluemix cf unmap-route Blue cf-react-cicd-demo.mybluemix.net -n cf-react-cicd-demo
+bluemix cf unmap-route Blue mybluemix.net -n cf-react-cicd-demo
 
 # Remove temporary route to new app
-bluemix cf unmap-route cf-react-cicd-demo-new
-bluemix cf delete-route cf-react-cicd-demo-new
+bluemix cf unmap-route Green mybluemix.net -n cf-react-cicd-demo-new
+bluemix cf delete-route mybluemix.net -n cf-react-cicd-demo-new -f
 
 # Delete old app
-bluemix cf delete cf-react-cicd-demo -f
+bluemix cf delete Blue -f
